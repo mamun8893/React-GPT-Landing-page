2d749c7556f642e7b0eacb3cd414eab689cd78ab
mamun8893 <0316361936@student.wub.edu.bd>
2022-04-13 00:42:29 +0600

    navbar design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import "./App.css";
+import { Navbar, Brand, Cta } from "./components";
+import {
+  Blog,
+  Feature,
+  Footer,
+  Header,
+  Posibility,
+  WhatGpt3,
+} from "./container";
+
 function App() {
   return (
-    <div>
-      <h2>Hello</h2>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGpt3 />
+      <Feature />
+      <Posibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 }
